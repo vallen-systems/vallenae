5232ab2ab84a5994629dbdb0925f9d2b48f2d611
Lukas Berbuer <lb@intra.vallen.de>
2019-10-09 11:42:32 +0200

    Code formatting with black

--- examples/time_picker.py
+++ examples/time_picker.py
@@ -1,8 +1,9 @@
 from __future__ import print_function, division, absolute_import
 
 import sys, os
+
 # find vallenae module in parent directory if not installed
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import math as m
 import matplotlib.pyplot as plt
@@ -20,9 +21,7 @@ matplotlib.interactive(False)  # "True" means plt.show() is non-blocking
 
 
 @jit(nb.types.Tuple((f8[:], f8, i4))(f8[:], i4))
-def energy_ratio(arr: np.ndarray,
-                 win_len: int = 100)\
-        -> Tuple[np.ndarray, float, int]:
+def energy_ratio(arr: np.ndarray, win_len: int = 100) -> Tuple[np.ndarray, float, int]:
     safetye_eps = 1e-6
     n = len(arr)
     er_vals = np.zeros(n)
@@ -31,14 +30,14 @@ def energy_ratio(arr: np.ndarray,
     min_val = m.inf
     min_idx = 0
     for k in range(0, win_len):
-        l_sumsq += arr[k]**2
-    for k in range(win_len, win_len+win_len):
-        r_sumsq += arr[k]**2
-    for k in range(win_len, n-win_len):
-        l_sumsq += arr[k]**2
-        r_sumsq += arr[k+win_len]**2
-        l_sumsq -= arr[k-win_len]**2
-        r_sumsq -= arr[k]**2
+        l_sumsq += arr[k] ** 2
+    for k in range(win_len, win_len + win_len):
+        r_sumsq += arr[k] ** 2
+    for k in range(win_len, n - win_len):
+        l_sumsq += arr[k] ** 2
+        r_sumsq += arr[k + win_len] ** 2
+        l_sumsq -= arr[k - win_len] ** 2
+        r_sumsq -= arr[k] ** 2
         er_vals[k] = r_sumsq / (safetye_eps + l_sumsq)
         if er_vals[k] < min_val:
             min_val = er_vals[k]
@@ -46,18 +45,18 @@ def energy_ratio(arr: np.ndarray,
     return er_vals, min_val, min_idx
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     THIS_FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 
     MAX_TRA_IDX = 4
-    END_SAMPLE  = 2000
-    WINDOW_LEN  = 100
+    END_SAMPLE = 2000
+    WINDOW_LEN = 100
     CONVERT_SECS_2_USECS = 1000000
 
     tra_frame = vae.read_tra(
-        os.path.join(
-            THIS_FILE_PATH, 'steel_plate/sample_plain.tradb'), 1, MAX_TRA_IDX)
+        os.path.join(THIS_FILE_PATH, "steel_plate/sample_plain.tradb"), 1, MAX_TRA_IDX
+    )
     ys, xs = vae.extract_wave(tra_frame, MAX_TRA_IDX, do_convert2v=True)
     ys = ys * 1000  # convert to mV
 
@@ -66,24 +65,20 @@ if __name__ == '__main__':
     start = time.perf_counter()
     er_arr, _, _ = energy_ratio(ys, WINDOW_LEN)
     end = time.perf_counter()
-    print('Runtime for 1 call to energy_ratio(): ', end - start)
+    print("Runtime for 1 call to energy_ratio(): ", end - start)
     # -------------------------------------------------------------
 
     # Code taken from the matplotlib documentation:
     fig, ax1 = plt.subplots()
-    ax1.set_xlabel('Time [us]')
-    ax1.set_ylabel('Tansient Wave [mV]', color='g')
-    ax1.plot(CONVERT_SECS_2_USECS * xs[0:END_SAMPLE],
-             ys[0:END_SAMPLE],
-             color='g')
-    ax1.tick_params(axis='y', labelcolor='g')
-    
+    ax1.set_xlabel("Time [us]")
+    ax1.set_ylabel("Tansient Wave [mV]", color="g")
+    ax1.plot(CONVERT_SECS_2_USECS * xs[0:END_SAMPLE], ys[0:END_SAMPLE], color="g")
+    ax1.tick_params(axis="y", labelcolor="g")
+
     ax2 = ax1.twinx()
-    ax2.set_ylabel('Energy Ratio [/]', color='r')
-    ax2.plot(CONVERT_SECS_2_USECS * xs[0:END_SAMPLE],
-             er_arr[0:END_SAMPLE],
-             color='r')
-    ax2.tick_params(axis='y', labelcolor='r')
+    ax2.set_ylabel("Energy Ratio [/]", color="r")
+    ax2.plot(CONVERT_SECS_2_USECS * xs[0:END_SAMPLE], er_arr[0:END_SAMPLE], color="r")
+    ax2.tick_params(axis="y", labelcolor="r")
 
     fig.tight_layout()
     plt.show()
--- vallenae/core.py
+++ vallenae/core.py
@@ -19,10 +19,7 @@ PLAIN_SPECIFIER = 0
 _version = __version_tpl__
 
 
-def read_pri(abs_file_name: str,
-             from_: int,
-             to_: int)\
-        -> pd.DataFrame:
+def read_pri(abs_file_name: str, from_: int, to_: int) -> pd.DataFrame:
     """
     Runs the following query on the pri-db file referenced by abs_file_name:
        SELECT * FROM view_tr_data
@@ -34,14 +31,11 @@ def read_pri(abs_file_name: str,
     :param to_: End SetID
     :returns: A pandas dataframe containing all available columns.
     """
-    data_frame = _read_db(abs_file_name, 'view_ae_data', 'SetID', from_, to_)
+    data_frame = _read_db(abs_file_name, "view_ae_data", "SetID", from_, to_)
     return data_frame
 
 
-def read_tra(abs_file_name: str,
-             from_: int,
-             to_: int)\
-        -> pd.DataFrame:
+def read_tra(abs_file_name: str, from_: int, to_: int) -> pd.DataFrame:
     """
     Runs the following query on the tra-db file referenced by abs_file_name:
        SELECT * FROM view_tr_data
@@ -53,15 +47,16 @@ def read_tra(abs_file_name: str,
     :param to_: End TRAI
     :returns: A pandas dataframe containing all available columns.
     """
-    data_frame = _read_db(abs_file_name, 'view_tr_data', 'TRAI', from_, to_)
+    data_frame = _read_db(abs_file_name, "view_tr_data", "TRAI", from_, to_)
     return data_frame
 
 
-def extract_wave(pd_tra_frame: pd.DataFrame,
-                 tra_idx: int,
-                 do_convert2v: bool = True,
-                 do_create_time_vector: bool = True)\
-        -> Tuple[np.ndarray, np.ndarray]:
+def extract_wave(
+    pd_tra_frame: pd.DataFrame,
+    tra_idx: int,
+    do_convert2v: bool = True,
+    do_create_time_vector: bool = True,
+) -> Tuple[np.ndarray, np.ndarray]:
     """
     Extracts a single waveform according to the given index.
 
@@ -72,46 +67,38 @@ def extract_wave(pd_tra_frame: pd.DataFrame,
         suitable time vector for the x-axis, i.e. for plotting?
     :returns: (scaled values in V, time vector in seconds)
     """
-    data_format = pd_tra_frame['DataFormat'][tra_idx]
+    data_format = pd_tra_frame["DataFormat"][tra_idx]
 
     if data_format not in {PLAIN_SPECIFIER, FLAC_SPECIFIER}:
-        raise TypeError("Unknown BLOB format. data_format = " +
-                        str(data_format))
+        raise TypeError("Unknown BLOB format. data_format = " + str(data_format))
 
     do_use_flac = data_format == FLAC_SPECIFIER
 
-    return _extract_wave(pd_tra_frame,
-                         tra_idx,
-                         do_use_flac,
-                         do_convert2v,
-                         do_create_time_vector)
+    return _extract_wave(
+        pd_tra_frame, tra_idx, do_use_flac, do_convert2v, do_create_time_vector
+    )
 
 
-def read_labels(abs_file_name: str)\
-        -> pd.DataFrame:
+def read_labels(abs_file_name: str) -> pd.DataFrame:
     """
     Read all the labels of the database file into a pandas dataframe.
 
     :param abs_file_name: Absolute path to the sqlite3 database file.
     :returns: The view_ae_markers table as pandas dataframe.
     """
-    query = 'SELECT SetID, Number, Data ' +\
-            'FROM view_ae_markers'
+    query = "SELECT SetID, Number, Data " + "FROM view_ae_markers"
     data_frame = None
     if os.path.exists(abs_file_name):
         with ctxlib.closing(sqlite3.connect(abs_file_name)) as db_conn:
             data_frame = pd.read_sql_query(query, db_conn)
     if data_frame is not None:
-        data_frame = data_frame.set_index('SetID')
+        data_frame = data_frame.set_index("SetID")
     return data_frame
 
 
-def _read_db(abs_file_name: str,
-             tbl_name: str,
-             indx: str,
-             from_: int,
-             to_: int)\
-        -> pd.DataFrame:
+def _read_db(
+    abs_file_name: str, tbl_name: str, indx: str, from_: int, to_: int
+) -> pd.DataFrame:
     """
     Internal helper function to read sqlite3 databases into a pandas dataframe.
         SELECT * FROM tbl_name WHERE SetID BETWEEN from_ AND to_
@@ -127,24 +114,26 @@ def _read_db(abs_file_name: str,
     # Table names can not be bound; at least we try to prevent
     # sql-injection in some way:
     if not TBL_NAME_PATTERN.match(tbl_name):
-        raise Exception('Illegal table name ' + tbl_name + '\n' +
-                        'Should consist just of letters and underscores.')
-    query = ('SELECT * FROM ' + tbl_name +
-             ' WHERE SetID >= ? AND SetID <= ?')
+        raise Exception(
+            "Illegal table name "
+            + tbl_name
+            + "\n"
+            + "Should consist just of letters and underscores."
+        )
+    query = "SELECT * FROM " + tbl_name + " WHERE SetID >= ? AND SetID <= ?"
     binding_params = (from_, to_)
     data_frame = None
     if os.path.exists(abs_file_name):
         with ctxlib.closing(sqlite3.connect(abs_file_name)) as db_conn:
-            data_frame = pd.read_sql_query(query, db_conn,
-                                           params=binding_params)
+            data_frame = pd.read_sql_query(query, db_conn, params=binding_params)
     if data_frame is not None:
         data_frame = data_frame.set_index(indx)
     return data_frame
 
 
-def _get_sr_pt_smpls_2mv(pd_tra_frame: pd.DataFrame,
-                         tra_idx: int)\
-        -> Tuple[int, int, int, float]:
+def _get_sr_pt_smpls_2mv(
+    pd_tra_frame: pd.DataFrame, tra_idx: int
+) -> Tuple[int, int, int, float]:
     """
     Internal helper function to extract the sample rate, the number of
     pre-trigger samples, the overall number of samples and the ADC to mV
@@ -155,18 +144,17 @@ def _get_sr_pt_smpls_2mv(pd_tra_frame: pd.DataFrame,
     :param tra_idx: The index of the waveform of interest.
     :returns (sample_rate, nb_pretrigs, nb_samples, raw2mv_factor):
     """
-    sample_rate = int(pd_tra_frame['SampleRate'][tra_idx])
-    nb_pretrigs = int(pd_tra_frame['Pretrigger'][tra_idx])
-    nb_samples = int(pd_tra_frame['Samples'][tra_idx])
-    raw2mv_factor = float(pd_tra_frame['TR_mV'][tra_idx])
+    sample_rate = int(pd_tra_frame["SampleRate"][tra_idx])
+    nb_pretrigs = int(pd_tra_frame["Pretrigger"][tra_idx])
+    nb_samples = int(pd_tra_frame["Samples"][tra_idx])
+    raw2mv_factor = float(pd_tra_frame["TR_mV"][tra_idx])
 
     return sample_rate, nb_pretrigs, nb_samples, raw2mv_factor
 
 
-def _create_time_vector(nb_pretrigs: int,
-                        nb_samples: int,
-                        sample_rate: int)\
-        -> np.ndarray:
+def _create_time_vector(
+    nb_pretrigs: int, nb_samples: int, sample_rate: int
+) -> np.ndarray:
     """
     Internal helper function to create a time base vector.
 
@@ -177,40 +165,45 @@ def _create_time_vector(nb_pretrigs: int,
         plots (x-axis).
     """
     raw_time_vec = np.arange(-nb_pretrigs, nb_samples - nb_pretrigs)
-    scaled_time_vec = np.multiply(raw_time_vec, 1.0/sample_rate)
+    scaled_time_vec = np.multiply(raw_time_vec, 1.0 / sample_rate)
 
     return scaled_time_vec
 
 
-def _extract_wave(pd_tra_frame: pd.DataFrame,
-                  tra_idx: int,
-                  do_use_flac: bool,
-                  do_convert2v: bool = True,
-                  do_create_time_vector: bool = True)\
-        -> Union[Tuple[np.ndarray, np.ndarray], np.ndarray]:
+def _extract_wave(
+    pd_tra_frame: pd.DataFrame,
+    tra_idx: int,
+    do_use_flac: bool,
+    do_convert2v: bool = True,
+    do_create_time_vector: bool = True,
+) -> Union[Tuple[np.ndarray, np.ndarray], np.ndarray]:
     """
     Helper function to extract little endian 16-bit adc values from a BLOB.
     Is capable of handling FLAC format.
     """
-    sr, nb_pretrigs, nb_smpls, to_mv_factor = \
-        _get_sr_pt_smpls_2mv(pd_tra_frame, tra_idx)
+    sr, nb_pretrigs, nb_smpls, to_mv_factor = _get_sr_pt_smpls_2mv(
+        pd_tra_frame, tra_idx
+    )
 
-    blob = pd_tra_frame['Data'][tra_idx]
+    blob = pd_tra_frame["Data"][tra_idx]
 
     if do_use_flac:
         result, _ = sf.read(io.BytesIO(blob))
         # the soundfile lib normalizes the adc values to +-1, that's why
         # we need this:
-        to_mv_factor = to_mv_factor * 2**15
+        to_mv_factor = to_mv_factor * 2 ** 15
     else:
-        result = np.array(arr.array('h', bytes(blob)))
+        result = np.array(arr.array("h", bytes(blob)))
 
     if do_convert2v:
-        result = np.multiply(1e-3*to_mv_factor, result)
+        result = np.multiply(1e-3 * to_mv_factor, result)
 
     if not do_create_time_vector:
         return result
     else:
-        return result, _create_time_vector(nb_pretrigs=nb_pretrigs,
-                                           nb_samples=nb_smpls,
-                                           sample_rate=sr)
+        return (
+            result,
+            _create_time_vector(
+                nb_pretrigs=nb_pretrigs, nb_samples=nb_smpls, sample_rate=sr
+            ),
+        )
