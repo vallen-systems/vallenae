1bb9526ed5c7b651c2c71ec0e50cfe8fe3395dfd
Lukas Berbuer <lukas.berbuer@gmail.com>
2020-01-17 09:17:25 +0100

    Add globalinfo inserts in trfdb schema

--- src/vallenae/io/schema_templates/trfdb.sql
+++ src/vallenae/io/schema_templates/trfdb.sql
@@ -17,3 +17,9 @@ CREATE TABLE trf_fieldinfo (
     "ShortName",
     "FormatStr"
 );
+
+-- Set globalinfo
+INSERT INTO trf_globalinfo (Key, Value) VALUES ("Version", 1);
+INSERT INTO trf_globalinfo (Key, Value) VALUES ("FileStatus", 0);
+INSERT INTO trf_globalinfo (Key, Value) VALUES ("WriterID", "-");
+INSERT INTO trf_globalinfo (Key, Value) VALUES ("ValidSets", 0);
