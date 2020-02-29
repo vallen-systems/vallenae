eae9b23f66292db8a5a05cb839701f46c0b1ca51
Lukas Berbuer <lukas.berbuer@gmail.com>
2020-02-29 01:49:06 +0100

    Add Python 3.8 to CI and setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        # "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
     ],
     keywords="vallen acoustic-emission amsy sqlite pridb tradb",
